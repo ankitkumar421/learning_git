1151d5ffd8ba0dc16394cc3d586896876d3215a1
ankit <ankitofficial421@gmail.com>
2022-05-02 23:49:21 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package com.git_learn;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a=5,b=6;
+        int sum=0;
+        sum=a+b;
+
+        System.out.println(sum);
+    }
+}

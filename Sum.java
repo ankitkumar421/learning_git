c500100f7907231d49be0325f8c60f42280a28e8
ankit <ankitofficial421@gmail.com>
2022-05-07 11:51:15 +0530

    added some new lines

--- Sum.java
+++ Sum.java
@@ -3,9 +3,10 @@ package com.git_learn;
 public class Sum {
     public static void main(String[] args) {
         int a=5,b=10;
-        int sum=0;
-        sum=a*b;
-
+        int sum = 0;
+        int mul = 0;
+        mul=a*b;
+        sum = a+b;
         System.out.println(sum);
     }
 }
